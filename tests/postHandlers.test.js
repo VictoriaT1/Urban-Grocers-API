a6dd6fcd018390190848fc40e497052bd3044d1c
Practicum Student <practicum_student@gmail.com>
2023-04-17 14:45:11 -0500

    commit change

--- tests/postHandlers.test.js
+++ tests/postHandlers.test.js
@@ -2,37 +2,52 @@
 const config = require('../config');
 
 const requestBody = {
-		"products": [
-			{
-				"id": 1,
-				"quantity": 3
-			},
-			{
-				"id": 4,
-				"quantity": 1
-			},
-			{
-				"id": 9,
-				"quantity": 3
-			}
-		],
-		"deliveryTime": 7
+	"productsList": [
+        {
+            "id": 1,
+            "quantity": 2
+        },
+        {
+            "id": 5,
+            "quantity": 2
+        },
+        {
+            "id": 3,
+            "quantity": 1
+        }
+    ]
 	}
 
-test('check courier delivery and status response as 200', async () => {
-	let postStatus;
+test('Status code should be 200', async () => {
+	let actualStatusCode;
     try {
-		const response = await fetch(`${config.API_URL}/api/v1/couriers/check`, {
-			method: 'POST',
-			headers: {
-			'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(requestBody)
-		});
-		postStatus = response.status;
-		
-	} catch (error) {
-		console.error(error);
-	}
-	expect(postStatus).toBe(200);
+		const response = await fetch(`${config.API_URL}/api/v1/orders`, {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody)
+        });
+        actualStatusCode = response.status;     
+    } catch (error) {
+        console.error(error);
+    }
+    expect(actualStatusCode).toBe(201);
 });
+
+test('Response body should contain Order and Go', async () => {
+	let actualResponseBody;
+    try {
+		const response = await fetch(`${config.API_URL}/api/v1/orders`, {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody)
+        });
+        actualResponseBody = await response.json();    
+    } catch (error) {
+        console.error(error);
+    }
+   
+}); expect(actualResponseBody["courierService"]).toBe("Order and Go");
